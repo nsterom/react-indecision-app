26fad87d58a01be74a137134281c14c4857f0655
mo141933 <lai_iekleong_mac@mail.notes.bank-of-china.com>
2020-03-23 13:16:50 +0800

    use jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,31 @@
 console.log('app.js is running!');
 
 var template = React.createElement(
-  'p',
-  null,
-  'this is a jsx from app.js!!'
+    'p',
+    null,
+    'this is a jsx from app.js!!'
+);
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Neil'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 30'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Macao'
+    )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ console.log('app.js is running!')
 
 var template = <p>this is a jsx from app.js!!</p>
 
+var templateTwo = (
+    <div>
+        <h1>Neil</h1>
+        <p>Age: 30</p>
+        <p>Location: Macao</p>
+    </div>
+)
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
